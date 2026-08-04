e42dfcb0d21dd35091334e3d00db95212b97933d
_guittaiki <guilhermetaiki5@gmail.com>
2026-08-04 09:14:19 -0300

    Trabalhando com condicional composta

--- 05-condicionais.php
+++ 05-condicionais.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Condicionais</title>
+    <style>
+        .comprar { color: red; }
+        .normal { color: pink; }
+    </style>
 </head>
 <body>
     <h1>Trabalhando com estruturas Condicionais</h1>
@@ -23,8 +27,32 @@
 
       // Estrutura alternativa (sem chaves, com : e endif)
       if($numero > 10):
-          echo "<p>$numero é maior que 10 .< /p>";
+          echo "<p>$numero é maior que 10 .</p>";
       endif;
-    ?>  
+    ?>
+    
+    <h2>Condicional COMPOSTA: <code>if/else</code></h2>
+    <?php  
+    $produto = "Ultrabook";
+    $qtdEmEstoque = 0;
+    $qtdCritica = 5;
+    ?>
+
+      <h3> <?= $produto ?></h3>
+      <p><b>Quantidade em estoque: </b> <?= $qtdEmEstoque ?></p>
+      
+    
+    <?php  
+      if ($qtdEmEstoque < $qtdCritica) {
+           echo "<p class=\"comprar\">É necessário comprar/repor</p>";
+
+           // Condicional ANINHADA (ela está dentro de outro if)
+           if($qtdEmEstoque === 0) {
+              echo "<p><mark class=\"comprar\">🚨URGENTE!</mark></p>";
+           }
+      } else {
+           echo "<p class=\"normal\">Estoque normal.</p>";
+      } 
+    ?>
 </body>
 </html>
