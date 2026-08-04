2171f7e784e8e9e2ce9c0d250faaf11678785a0b
_guittaiki <guilhermetaiki5@gmail.com>
2026-08-04 11:09:54 -0300

    Versão usando endif e :

--- 06-condicional-refatorada.php
+++ 06-condicional-refatorada.php
@@ -37,20 +37,20 @@
       
     
     <?php  
-      if ($qtdEmEstoque < $qtdCritica) {
+      if ($qtdEmEstoque < $qtdCritica) :
     ?>
           <p class="comprar">É necessário comprar/repor</p>
     <?php              
-           if($qtdEmEstoque === 0) {
+           if($qtdEmEstoque === 0) :
     ?>          
            <p><mark class="comprar">🚨URGENTE!</mark></p>
     <?php  
-           }       
-         } else {
+            endif;       
+         else:
     ?>
               <p class="normal">Estoque normal</p>         
     <?php
-         }
+         endif;
     ?>     
 
     <h3>Condicional ENCADEADA: <code>if, eslseif, else</code></h3>
