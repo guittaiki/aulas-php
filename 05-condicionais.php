65d35d9b931d3cd4bc06475e3878adb3cd207de6
_guittaiki <guilhermetaiki5@gmail.com>
2026-08-04 08:44:24 -0300

    Exemplos de condicionais simples

--- 05-condicionais.php
+++ 05-condicionais.php
@@ -8,6 +8,23 @@
 <body>
     <h1>Trabalhando com estruturas Condicionais</h1>
     <hr>
-    
+
+    <h2>Condicional SIMPLES: <code>if</code></h2>
+    <?php  
+      $numero = 50;
+
+      // Estrutura tradicional (comando, parênteses, chaves)
+      if($numero > 10){
+        echo "<p>$numero é maior que 10.</p>";
+      }
+
+      // Estrutura abreviada (sem chaves)
+      if($numero > 10) echo "<p>$numero é maior que 10.</p>";
+
+      // Estrutura alternativa (sem chaves, com : e endif)
+      if($numero > 10):
+          echo "<p>$numero é maior que 10 .< /p>";
+      endif;
+    ?>  
 </body>
 </html>
